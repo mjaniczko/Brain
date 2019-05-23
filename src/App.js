9bb16b6b5fd4447efe5e9204f9e35f2ba5ed14af
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-23 07:54:11 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Particles from 'react-particles-js'
+
+import Navigation from './components/Navigation/Navigation'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import Logo from './components/Logo/Logo'
+
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 251,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles'
+        params={particlesOptions}
+      />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
