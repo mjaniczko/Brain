a19200917caed8118c8cec70dc6f9a6094b9c3ae
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-27 10:33:49 +0200

    Change onButtomSubmit into onPictureSubmit and add entries

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const app = new Clarifai.App({
   apiKey: '7d93a68c97104ae39d676ef68da33d26'
  })
 
-
 const particlesOptions = {
   particles: {
     number: {
@@ -43,7 +42,7 @@ class App extends Component {
         id: '',
         name: '',
         email: '',
-        entreis: 0,
+        entries: 0,
         joined: ''
       }
     }
@@ -54,7 +53,7 @@ class App extends Component {
       id: data.id,
       name: data.name,
       email: data.email,
-      entreis: data.entreis,
+      entries: data.entries,
       joined: data.joined
     }})
   }
@@ -80,13 +79,27 @@ class App extends Component {
     this.setState({ input: e.target.value })
   }
 
-  onButtonSubmit = () => {
+  onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input })
     app.models
       .predict(
         Clarifai.FACE_DETECT_MODEL,
         this.state.input)
-      .then(response => this.dispalyFaceBox(this.calculateFaceLocation(response)))
+      .then(response => {
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count }))
+          })
+        }
+        this.dispalyFaceBox(this.calculateFaceLocation(response))})
       .catch(err => console.log(err))
   }
 
@@ -100,7 +113,7 @@ class App extends Component {
   }
 
   render () {
-    const { isSignedIn, imageUrl, route, box } = this.state
+    const { isSignedIn, imageUrl, route, box, user } = this.state
     return (
       <div className="App">
         <Particles className='particles'
@@ -110,8 +123,8 @@ class App extends Component {
         { route === 'home'
           ? <div>
               <Logo />
-              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
-              <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+              <Rank name={user.name} entries={user.entries} />
+              <ImageLinkForm onInputChange={this.onInputChange} onPictureSubmit={this.onPictureSubmit} />
               <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
           : (
@@ -121,8 +134,8 @@ class App extends Component {
           )
         }
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
