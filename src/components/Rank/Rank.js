c036bb1718e2212f5a9aeac5a46b445493c4a68b
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-27 10:36:12 +0200

    Change message into clearer

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 
-const Rank = ({ name, entries} ) => {
+const Rank = ({ name, entries }) => {
   return (
     <div>
       <div className='white f3'>
-        {`${name} , your current rank is...`}
+        {`${name} , your current entry count is...`}
       </div>
       <div className='white f1'>
         {entries}
