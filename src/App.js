72ca5b775ea6b323f39b848d0ac9754b1d7c4cce
Michał Janiczko <michaljaniczko1@gmail.com>
2019-05-27 08:47:01 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
     }
   }
 
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
     const image = document.getElementById('inputimage')
